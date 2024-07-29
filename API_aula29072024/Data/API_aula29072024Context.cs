0bb697f02ae363ef4824ba765cef2db0d76a2812
anasgomess <ana.s.gomes2007@gmail.com>
2024-07-29 08:50:45 -0300

    Aula de 2024

---
+++ API_aula29072024/Data/API_aula29072024Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API_aula29072024.Models;
+
+namespace API_aula29072024.Data
+{
+    public class API_aula29072024Context : DbContext
+    {
+        public API_aula29072024Context (DbContextOptions<API_aula29072024Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<API_aula29072024.Models.Produto> Produto { get; set; }
+    }
+}
---
+++ API_aula29072024/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace API_aula29072024.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Status { get; set; }
+    }
+}
